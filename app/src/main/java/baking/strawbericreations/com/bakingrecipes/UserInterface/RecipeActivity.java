690e8a4953120f8e6fe44bd0035a2ed2757a9d2d
humaf <qfhuma@gmail.com>
2018-01-17 12:17:35 -0800

    Initial commit

---
+++ app/src/main/java/baking/strawbericreations/com/bakingrecipes/UserInterface/RecipeActivity.java
@@ -0,0 +1,13 @@
+package baking.strawbericreations.com.bakingrecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
